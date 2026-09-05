fd6e20b87130fd44ca4bb362d76c9a7e79862efa
PilarFernandezNieto <apreilmc@gmail.com>
2026-09-05 21:19:09 +0200

    navegación

--- recetas-frontend/src/layouts/GuestLayout.vue
+++ recetas-frontend/src/layouts/GuestLayout.vue
@@ -24,17 +24,40 @@ const showingNavigationDropdown = ref(false)
     <!-- Navegación principal -->
     <nav class="bg-papel border-b border-antracita/10">
       <div class="max-w-7xl mx-auto p-4 sm:px-6 lg:px-8">
-        <div class="flex flex-col">
-          <!-- Logo -->
-          <div class="flex items-center shrink-0 justify-center">
-            <RouterLink :to="{ name: 'home' }">
-              <ApplicationLogo class="block w-40" />
-            </RouterLink>
-          </div>
-          <!-- Navigation Links -->
-          <div class="flex">
-            <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-              <!-- <template v-if="!isLoggedIn">
+        <!-- Hamburger -->
+        <div class="-mr-2 flex items-center justify-end sm:hidden">
+          <button
+            @click="showingNavigationDropdown = !showingNavigationDropdown"
+            class="inline-flex items-center justify-center p-2 rounded-calido text-verde-900/60 hover:text-verde hover:bg-papel focus:outline-none focus:bg-papel focus:text-verde transition duration-150 ease-in-out"
+          >
+            <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+              <path
+                :class="{
+                  hidden: showingNavigationDropdown,
+                  'inline-flex': !showingNavigationDropdown,
+                }"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+              <path
+                :class="{
+                  hidden: !showingNavigationDropdown,
+                  'inline-flex': showingNavigationDropdown,
+                }"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </div>
+        <!-- Navigation Links -->
+        <div class="flex justify-end">
+          <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+            <!-- <template v-if="!isLoggedIn">
                 <NavLink
                   :to="{ name: 'login' }"
                   class="font-semibold text-gray-600 hover:text-gray-900"
@@ -46,76 +69,51 @@ const showingNavigationDropdown = ref(false)
                   >Registro</NavLink
                 >
               </template> -->
-            </div>
-            <div class="hidden sm:flex sm:items-center sm:ml-6">
-              <!-- Settings Dropdown User-->
-              <div v-if="isLoggedIn" class="ml-3 relative">
-                <Dropdown align="right" width="48">
-                  <template #trigger>
-                    <span class="inline-flex rounded-md">
-                      <button
-                        type="button"
-                        class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-principal font-medium rounded-calido text-verde-900 bg-crema hover:bg-verde hover:text-crema focus:outline-none transition ease-in-out duration-150"
+          </div>
+          <div class="hidden sm:flex sm:items-center sm:ml-6">
+            <!-- Settings Dropdown User-->
+            <div v-if="isLoggedIn" class="ml-3 relative">
+              <Dropdown align="right" width="48">
+                <template #trigger>
+                  <span class="inline-flex rounded-sm">
+                    <button
+                      type="button"
+                      class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-principal font-medium rounded-sm text-verde-900 bg-crema hover:bg-verde hover:text-crema focus:outline-none transition ease-in-out duration-150"
+                    >
+                      {{ user?.name }}
+
+                      <svg
+                        class="ml-2 -mr-0.5 h-4 w-4"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
                       >
-                        {{ user?.name }}
+                        <path
+                          fill-rule="evenodd"
+                          d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                          clip-rule="evenodd"
+                        />
+                      </svg>
+                    </button>
+                  </span>
+                </template>
+                <template #content>
+                  <div v-if="isAdmin">
+                    <DropdownLink :to="{ name: 'dashboard' }">Administrador</DropdownLink>
+                  </div>
 
-                        <svg
-                          class="ml-2 -mr-0.5 h-4 w-4"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                        >
-                          <path
-                            fill-rule="evenodd"
-                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                            clip-rule="evenodd"
-                          />
-                        </svg>
-                      </button>
-                    </span>
-                  </template>
-                  <template #content>
-                    <div v-if="isAdmin">
-                      <DropdownLink :to="{ name: 'dashboard' }">Administrador</DropdownLink>
-                    </div>
-
-                    <DropdownButton @click="logout()">Cierra sesión</DropdownButton>
-                  </template>
-                </Dropdown>
-              </div>
+                  <DropdownButton @click="logout()">Cierra sesión</DropdownButton>
+                </template>
+              </Dropdown>
             </div>
           </div>
+        </div>
 
-          <!-- Hamburger -->
-          <div class="-mr-2 flex items-center sm:hidden">
-            <button
-              @click="showingNavigationDropdown = !showingNavigationDropdown"
-              class="inline-flex items-center justify-center p-2 rounded-calido text-verde-900/60 hover:text-verde hover:bg-papel focus:outline-none focus:bg-papel focus:text-verde transition duration-150 ease-in-out"
-            >
-              <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                <path
-                  :class="{
-                    hidden: showingNavigationDropdown,
-                    'inline-flex': !showingNavigationDropdown,
-                  }"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-                <path
-                  :class="{
-                    hidden: !showingNavigationDropdown,
-                    'inline-flex': showingNavigationDropdown,
-                  }"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </button>
-          </div>
+        <!-- Logo -->
+        <div class="flex items-center justify-center">
+          <RouterLink :to="{ name: 'home' }">
+            <ApplicationLogo class="block w-40" />
+          </RouterLink>
         </div>
       </div>
 
